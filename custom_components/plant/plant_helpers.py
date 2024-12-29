00ba1b807b351cc68d73f95b14b16457ed56ba80
dingausmwald <30699246+dingausmwald@users.noreply.github.com>
2024-12-29 20:19:45 +0100

    Update plant_helpers.py

--- custom_components/plant/plant_helpers.py
+++ custom_components/plant/plant_helpers.py
@@ -1,53 +1,40 @@
-"""Helper functions for the plant integration"""
-
+"""Plant helper functions"""
 from __future__ import annotations
 
 import logging
 from typing import Any
 
-from async_timeout import timeout
-import voluptuous as vol
-
-from homeassistant.components.persistent_notification import (
-    create as create_notification,
-)
-from homeassistant.const import ATTR_ENTITY_PICTURE, ATTR_NAME, UnitOfTemperature
+from homeassistant.const import ATTR_ENTITY_PICTURE, ATTR_NAME
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers import config_validation as cv
-from homeassistant.helpers.temperature import display_temp
 
 from .const import (
+    ATTR_BREEDER,
     ATTR_BRIGHTNESS,
     ATTR_CONDUCTIVITY,
+    ATTR_FLOWERING_DURATION,
+    ATTR_HUMIDITY,
     ATTR_ILLUMINANCE,
-    ATTR_IMAGE,
     ATTR_LIMITS,
     ATTR_MOISTURE,
+    ATTR_PID,
     ATTR_SENSORS,
-    ATTR_SPECIES,
+    ATTR_STRAIN,
     ATTR_TEMPERATURE,
-    CONF_MAX_BRIGHTNESS,
     CONF_MAX_CONDUCTIVITY,
     CONF_MAX_DLI,
     CONF_MAX_HUMIDITY,
     CONF_MAX_ILLUMINANCE,
-    CONF_MAX_MMOL,
     CONF_MAX_MOISTURE,
     CONF_MAX_TEMPERATURE,
-    CONF_MIN_BRIGHTNESS,
     CONF_MIN_CONDUCTIVITY,
     CONF_MIN_DLI,
     CONF_MIN_HUMIDITY,
     CONF_MIN_ILLUMINANCE,
-    CONF_MIN_MMOL,
     CONF_MIN_MOISTURE,
     CONF_MIN_TEMPERATURE,
-    CONF_PLANTBOOK_MAPPING,
     DATA_SOURCE,
-    DATA_SOURCE_DEFAULT,
+    DATA_SOURCE_MANUAL,
     DATA_SOURCE_PLANTBOOK,
-    DEFAULT_IMAGE_LOCAL_URL,
-    DEFAULT_IMAGE_PATH,
     DEFAULT_MAX_CONDUCTIVITY,
     DEFAULT_MAX_DLI,
     DEFAULT_MAX_HUMIDITY,
@@ -61,287 +48,155 @@ from .const import (
     DEFAULT_MIN_MOISTURE,
     DEFAULT_MIN_TEMPERATURE,
     DOMAIN_PLANTBOOK,
-    FLOW_FORCE_SPECIES_UPDATE,
-    FLOW_PLANT_IMAGE,
     FLOW_PLANT_INFO,
-    FLOW_SENSOR_CONDUCTIVITY,
-    FLOW_SENSOR_ILLUMINANCE,
-    FLOW_SENSOR_MOISTURE,
-    FLOW_SENSOR_TEMPERATURE,
     OPB_DISPLAY_PID,
     OPB_GET,
-    OPB_SEARCH,
-    PPFD_DLI_FACTOR,
-    REQUEST_TIMEOUT,
+    DEVICE_TYPE_CYCLE,
+    ATTR_DEVICE_TYPE,
 )
 
 _LOGGER = logging.getLogger(__name__)
 
-
 class PlantHelper:
-    """Helper functions for the plant integration"""
+    """Helper class for plant component."""
 
     def __init__(self, hass: HomeAssistant) -> None:
-        self.hass = hass
+        """Initialize the helper."""
+        self._hass = hass
+        self.has_openplantbook = DOMAIN_PLANTBOOK in hass.config.components
 
-    @property
-    def has_openplantbook(self) -> bool:
-        """Helper function to check if openplantbook is available"""
-        _LOGGER.debug(
-            "%s in services? %s",
-            DOMAIN_PLANTBOOK,
-            DOMAIN_PLANTBOOK in self.hass.services.async_services(),
-        )
-        return DOMAIN_PLANTBOOK in self.hass.services.async_services()
-
-    async def openplantbook_search(self, species: str) -> dict[str:Any] | None:
-        """Search OPB and return list of result"""
-
+    async def get_plantbook_data(self, config: dict) -> dict:
+        """Get plant data from OpenPlantbook."""
         if not self.has_openplantbook:
-            return None
-        if not species or species == "":
-            return None
+            return {}
+
+        strain = config.get(ATTR_STRAIN)
+        breeder = config.get(ATTR_BREEDER)
+        if not strain:
+            return {}
 
         try:
-            async with timeout(REQUEST_TIMEOUT):
-                plant_search_result = await self.hass.services.async_call(
-                    domain=DOMAIN_PLANTBOOK,
-                    service=OPB_SEARCH,
-                    service_data={"alias": species},
-                    blocking=True,
-                    return_response=True,
-                )
-        except TimeoutError:
-            _LOGGER.warning("Openplantook request timed out")
-            return None
+            result = await self._hass.services.async_call(
+                DOMAIN_PLANTBOOK,
+                OPB_GET,
+                {
+                    "species": strain,
+                    "breeder": breeder
+                },
+                blocking=True,
+                return_response=True
+            )
+            
+            if result:
+                _LOGGER.debug("Raw OpenPlantbook response: %s", result)
+                ret = {}
+                ret[FLOW_PLANT_INFO] = {
+                    DATA_SOURCE: DATA_SOURCE_PLANTBOOK,
+                    ATTR_PID: result.get("pid", ""),
+                    ATTR_STRAIN: result.get("strain", strain),
+                    ATTR_BREEDER: result.get("breeder", breeder),
+                    ATTR_ENTITY_PICTURE: result.get("image_url", ""),
+                    OPB_DISPLAY_PID: result.get("strain", ""),
+                    ATTR_FLOWERING_DURATION: int(result.get("flowertime", "0")),
+                    "sorte": result.get("sorte", ""),
+                    "feminized": result.get("feminized", ""),
+                    "timestamp": result.get("timestamp", ""),
+                    "website": result.get("website", ""),
+                    "infotext1": result.get("infotext1", ""),
+                    "infotext2": result.get("infotext2", ""),
+                    "effects": result.get("effects", ""),
+                    "smell": result.get("smell", ""),
+                    "taste": result.get("taste", ""),
+                    "lineage": result.get("lineage", ""),
+                }
+
+                # Add default limits
+                ret[FLOW_PLANT_INFO][ATTR_LIMITS] = {
+                    CONF_MAX_MOISTURE: DEFAULT_MAX_MOISTURE,
+                    CONF_MIN_MOISTURE: DEFAULT_MIN_MOISTURE,
+                    CONF_MAX_ILLUMINANCE: DEFAULT_MAX_ILLUMINANCE,
+                    CONF_MIN_ILLUMINANCE: DEFAULT_MIN_ILLUMINANCE,
+                    CONF_MAX_TEMPERATURE: DEFAULT_MAX_TEMPERATURE,
+                    CONF_MIN_TEMPERATURE: DEFAULT_MIN_TEMPERATURE,
+                    CONF_MAX_CONDUCTIVITY: DEFAULT_MAX_CONDUCTIVITY,
+                    CONF_MIN_CONDUCTIVITY: DEFAULT_MIN_CONDUCTIVITY,
+                    CONF_MAX_HUMIDITY: DEFAULT_MAX_HUMIDITY,
+                    CONF_MIN_HUMIDITY: DEFAULT_MIN_HUMIDITY,
+                    CONF_MAX_DLI: DEFAULT_MAX_DLI,
+                    CONF_MIN_DLI: DEFAULT_MIN_DLI,
+                }
+                return ret
+
         except Exception as ex:
-            _LOGGER.warning("Openplantook does not work, error: %s", ex)
-            return None
-        if bool(plant_search_result):
-            _LOGGER.info("Result: %s", plant_search_result)
-
-            return plant_search_result
-        return None
-
-    async def openplantbook_get(self, species: str) -> dict[str:Any] | None:
-        """Get information about a plant species from OpenPlantbook"""
-        if not self.has_openplantbook:
-            return None
-        if not species or species == "":
-            return None
-
-        try:
-            async with timeout(REQUEST_TIMEOUT):
-                plant_get_result = await self.hass.services.async_call(
-                    domain=DOMAIN_PLANTBOOK,
-                    service=OPB_GET,
-                    service_data={ATTR_SPECIES: species.lower()},
-                    blocking=True,
-                    return_response=True,
-                )
-        except TimeoutError:
-            _LOGGER.warning("Openplantook request timed out")
-        except Exception as ex:
-            _LOGGER.warning("Openplantook does not work, error: %s", ex)
-            return None
-        if bool(plant_get_result):
-            _LOGGER.debug("Result for %s: %s", species, plant_get_result)
-            return plant_get_result
-
-        _LOGGER.info("Did not find '%s' in OpenPlantbook", species)
-        create_notification(
-            hass=self.hass,
-            title="Species not found",
-            message=f"Could not find «{species}» in OpenPlantbook.",
-        )
-        return None
+            _LOGGER.warning("Unable to get OpenPlantbook data: %s", ex)
+            
+        return {}
 
     async def generate_configentry(self, config: dict) -> dict[str:Any]:
-        """Generates a config-entry dict from current data and/or OPB"""
+        """Generate a config entry."""
+        _LOGGER.debug("Generating config entry for %s", config)
+        ret = {}
+        ret[FLOW_PLANT_INFO] = {}
 
-        max_moisture = DEFAULT_MAX_MOISTURE
-        min_moisture = DEFAULT_MIN_MOISTURE
-        max_light_lx = DEFAULT_MAX_ILLUMINANCE
-        min_light_lx = DEFAULT_MIN_ILLUMINANCE
-        max_temp = display_temp(
-            self.hass,
-            DEFAULT_MAX_TEMPERATURE,
-            UnitOfTemperature.CELSIUS,
-            0,
-        )
-        min_temp = display_temp(
-            self.hass,
-            DEFAULT_MIN_TEMPERATURE,
-            UnitOfTemperature.CELSIUS,
-            0,
-        )
-        max_conductivity = DEFAULT_MAX_CONDUCTIVITY
-        min_conductivity = DEFAULT_MIN_CONDUCTIVITY
-        max_dli = DEFAULT_MAX_DLI
-        min_dli = DEFAULT_MIN_DLI
-        max_humidity = DEFAULT_MAX_HUMIDITY
-        min_humidity = DEFAULT_MIN_HUMIDITY
-        entity_picture = None
-        display_species = None
-        data_source = DATA_SOURCE_DEFAULT
+        # Get OpenPlantbook data if available
+        if self.has_openplantbook and config.get(ATTR_DEVICE_TYPE) != DEVICE_TYPE_CYCLE:
+            opb_config = await self.get_plantbook_data(config)
+            if opb_config:
+                ret.update(opb_config)
+                return ret
 
-        # If we have image defined in the config, or a local file
-        # prefer that.  If neither, image will be set to openplantbook
-        jpeg_exists = None
-        png_exists = None
-
-        if ATTR_SPECIES in config:
-            try:
-                jpeg_exists = cv.isfile(
-                    f"{DEFAULT_IMAGE_PATH}{config[ATTR_SPECIES]}.jpg"
-                )
-            except vol.Invalid:
-                jpeg_exists = None
-            try:
-                png_exists = cv.isfile(
-                    f"{DEFAULT_IMAGE_PATH}{config[ATTR_SPECIES]}.png"
-                )
-            except vol.Invalid:
-                png_exists = None
-
-        if ATTR_ENTITY_PICTURE in config:
-            entity_picture = config[ATTR_ENTITY_PICTURE]
-        elif ATTR_IMAGE in config and config[ATTR_IMAGE] != DOMAIN_PLANTBOOK:
-            entity_picture = config[ATTR_IMAGE]
-        elif jpeg_exists:
-            entity_picture = f"{DEFAULT_IMAGE_LOCAL_URL}{config[ATTR_SPECIES]}.jpg"
-        elif png_exists:
-            entity_picture = f"{DEFAULT_IMAGE_LOCAL_URL}{config[ATTR_SPECIES]}.png"
-
-        if ATTR_SENSORS not in config:
-            config[ATTR_SENSORS] = {}
-
-        if config.get(OPB_DISPLAY_PID, "") == "":
-            config[OPB_DISPLAY_PID] = None
-        opb_plant = await self.openplantbook_get(config.get(ATTR_SPECIES))
-        if opb_plant:
-            data_source = DATA_SOURCE_PLANTBOOK
-            max_moisture = opb_plant.get(
-                CONF_PLANTBOOK_MAPPING[CONF_MAX_MOISTURE], DEFAULT_MAX_MOISTURE
-            )
-            min_moisture = opb_plant.get(
-                CONF_PLANTBOOK_MAPPING[CONF_MIN_MOISTURE], DEFAULT_MIN_MOISTURE
-            )
-            max_light_lx = opb_plant.get(
-                CONF_PLANTBOOK_MAPPING[CONF_MAX_ILLUMINANCE],
-                DEFAULT_MAX_ILLUMINANCE,
-            )
-            min_light_lx = opb_plant.get(
-                CONF_PLANTBOOK_MAPPING[CONF_MIN_ILLUMINANCE],
-                DEFAULT_MIN_ILLUMINANCE,
-            )
-            max_temp = display_temp(
-                self.hass,
-                opb_plant.get(
-                    CONF_PLANTBOOK_MAPPING[CONF_MAX_TEMPERATURE],
-                    DEFAULT_MAX_TEMPERATURE,
-                ),
-                UnitOfTemperature.CELSIUS,
-                0,
-            )
-            min_temp = display_temp(
-                self.hass,
-                opb_plant.get(
-                    CONF_PLANTBOOK_MAPPING[CONF_MIN_TEMPERATURE],
-                    DEFAULT_MIN_TEMPERATURE,
-                ),
-                UnitOfTemperature.CELSIUS,
-                0,
-            )
-            opb_mmol = opb_plant.get(CONF_PLANTBOOK_MAPPING[CONF_MAX_MMOL])
-            if opb_mmol:
-                max_dli = round(opb_mmol * PPFD_DLI_FACTOR)
-            else:
-                max_dli = DEFAULT_MAX_DLI
-            opb_mmol = opb_plant.get(CONF_PLANTBOOK_MAPPING[CONF_MIN_MMOL])
-            if opb_mmol:
-                min_dli = round(opb_mmol * PPFD_DLI_FACTOR)
-            else:
-                min_dli = DEFAULT_MIN_DLI
-            max_conductivity = opb_plant.get(
-                CONF_PLANTBOOK_MAPPING[CONF_MAX_CONDUCTIVITY],
-                DEFAULT_MAX_CONDUCTIVITY,
-            )
-            min_conductivity = opb_plant.get(
-                CONF_PLANTBOOK_MAPPING[CONF_MIN_CONDUCTIVITY],
-                DEFAULT_MIN_CONDUCTIVITY,
-            )
-            max_humidity = opb_plant.get(
-                CONF_PLANTBOOK_MAPPING[CONF_MAX_HUMIDITY], DEFAULT_MAX_HUMIDITY
-            )
-            min_humidity = opb_plant.get(
-                CONF_PLANTBOOK_MAPPING[CONF_MIN_HUMIDITY], DEFAULT_MIN_HUMIDITY
-            )
-            _LOGGER.info("Picture: %s", entity_picture)
-            if (
-                entity_picture is None
-                or entity_picture == ""
-                or "plantbook.io" in entity_picture
-                or (
-                    FLOW_FORCE_SPECIES_UPDATE in config
-                    and config[FLOW_FORCE_SPECIES_UPDATE] is True
-                )
-            ):
-                entity_picture = opb_plant.get(FLOW_PLANT_IMAGE)
-            if (
-                FLOW_FORCE_SPECIES_UPDATE in config
-                and config[FLOW_FORCE_SPECIES_UPDATE] is True
-            ):
-                display_species = opb_plant.get(OPB_DISPLAY_PID, "")
-            else:
-                _LOGGER.debug(
-                    "Setting display_pid to %s",
-                    config.get(OPB_DISPLAY_PID) or opb_plant.get(OPB_DISPLAY_PID, ""),
-                )
-                display_species = config.get(OPB_DISPLAY_PID) or opb_plant.get(
-                    OPB_DISPLAY_PID, ""
-                )
-
-        _LOGGER.debug("Parsing input config: %s", config)
-        _LOGGER.debug("Display pid: %s", display_species)
-
-        ret = {
-            DATA_SOURCE: data_source,
-            FLOW_PLANT_INFO: {
-                ATTR_NAME: config.get(ATTR_NAME),
-                ATTR_SPECIES: config.get(ATTR_SPECIES) or "",
-                ATTR_ENTITY_PICTURE: entity_picture or "",
-                OPB_DISPLAY_PID: display_species or "",
-                ATTR_LIMITS: {
-                    CONF_MAX_ILLUMINANCE: config.get(
-                        CONF_MAX_BRIGHTNESS,
-                        config.get(CONF_MAX_ILLUMINANCE, max_light_lx),
-                    ),
-                    CONF_MIN_ILLUMINANCE: config.get(
-                        CONF_MIN_BRIGHTNESS,
-                        config.get(CONF_MIN_ILLUMINANCE, min_light_lx),
-                    ),
-                    CONF_MAX_CONDUCTIVITY: config.get(
-                        CONF_MAX_CONDUCTIVITY, max_conductivity
-                    ),
-                    CONF_MIN_CONDUCTIVITY: config.get(
-                        CONF_MIN_CONDUCTIVITY, min_conductivity
-                    ),
-                    CONF_MAX_MOISTURE: config.get(CONF_MAX_MOISTURE, max_moisture),
-                    CONF_MIN_MOISTURE: config.get(CONF_MIN_MOISTURE, min_moisture),
-                    CONF_MAX_TEMPERATURE: config.get(CONF_MAX_TEMPERATURE, max_temp),
-                    CONF_MIN_TEMPERATURE: config.get(CONF_MIN_TEMPERATURE, min_temp),
-                    CONF_MAX_HUMIDITY: config.get(CONF_MAX_HUMIDITY, max_humidity),
-                    CONF_MIN_HUMIDITY: config.get(CONF_MIN_HUMIDITY, min_humidity),
-                    CONF_MAX_DLI: config.get(CONF_MAX_DLI, max_dli),
-                    CONF_MIN_DLI: config.get(CONF_MIN_DLI, min_dli),
-                },
-                FLOW_SENSOR_TEMPERATURE: config[ATTR_SENSORS].get(ATTR_TEMPERATURE),
-                FLOW_SENSOR_MOISTURE: config[ATTR_SENSORS].get(ATTR_MOISTURE),
-                FLOW_SENSOR_CONDUCTIVITY: config[ATTR_SENSORS].get(ATTR_CONDUCTIVITY),
-                FLOW_SENSOR_ILLUMINANCE: config[ATTR_SENSORS].get(ATTR_ILLUMINANCE)
-                or config[ATTR_SENSORS].get(ATTR_BRIGHTNESS),
-            },
+        # Basis-Attribute für beide Typen
+        base_info = {
+            ATTR_NAME: config[ATTR_NAME],
+            ATTR_STRAIN: config.get(ATTR_STRAIN, ""),
+            ATTR_BREEDER: config.get(ATTR_BREEDER, ""),
+            DATA_SOURCE: DATA_SOURCE_MANUAL,
+            ATTR_ENTITY_PICTURE: config.get(ATTR_ENTITY_PICTURE, ""),
+            OPB_DISPLAY_PID: config.get(OPB_DISPLAY_PID, ""),
+            ATTR_FLOWERING_DURATION: config.get(ATTR_FLOWERING_DURATION, "0"),
+            "pid": "",
+            "sorte": "",
+            "feminized": "",
+            "timestamp": "",
+            "website": "",
+            "effects": "",
+            "smell": "",
+            "taste": "",
+            "phenotype": "",
+            "hunger": "",
+            "growth_stretch": "",
+            "flower_stretch": "",
+            "mold_resistance": "",
+            "difficulty": "",
+            "yield": "",
+            "notes": "",
         }
+
+        # Füge lineage und infotexte nur für Plants hinzu
+        if config.get(ATTR_DEVICE_TYPE) != DEVICE_TYPE_CYCLE:
+            base_info.update({
+                "lineage": "",
+                "infotext1": "",
+                "infotext2": "",
+            })
+
+        ret[FLOW_PLANT_INFO] = base_info
+
+        # Füge die Standard-Grenzwerte hinzu
+        ret[FLOW_PLANT_INFO][ATTR_LIMITS] = {
+            CONF_MAX_MOISTURE: DEFAULT_MAX_MOISTURE,
+            CONF_MIN_MOISTURE: DEFAULT_MIN_MOISTURE,
+            CONF_MAX_ILLUMINANCE: DEFAULT_MAX_ILLUMINANCE,
+            CONF_MIN_ILLUMINANCE: DEFAULT_MIN_ILLUMINANCE,
+            CONF_MAX_TEMPERATURE: DEFAULT_MAX_TEMPERATURE,
+            CONF_MIN_TEMPERATURE: DEFAULT_MIN_TEMPERATURE,
+            CONF_MAX_CONDUCTIVITY: DEFAULT_MAX_CONDUCTIVITY,
+            CONF_MIN_CONDUCTIVITY: DEFAULT_MIN_CONDUCTIVITY,
+            CONF_MAX_HUMIDITY: DEFAULT_MAX_HUMIDITY,
+            CONF_MIN_HUMIDITY: DEFAULT_MIN_HUMIDITY,
+            CONF_MAX_DLI: DEFAULT_MAX_DLI,
+            CONF_MIN_DLI: DEFAULT_MIN_DLI,
+        }
+
         _LOGGER.debug("Resulting config: %s", ret)
         return ret
